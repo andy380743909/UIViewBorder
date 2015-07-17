9bc48512d1908a90b35ceb1f31d4db8b5e992393
CuiPanJun <cuipanjun@mmbang.net>
2015-07-17 16:12:32 +0800

    init commit

--- UIViewBorderDemo/DetailViewController.m
+++ UIViewBorderDemo/DetailViewController.m
@@ -8,6 +8,8 @@
 
 #import "DetailViewController.h"
 
+#import "UIView+Border.h"
+
 @interface DetailViewController ()
 
 @end
@@ -30,6 +32,82 @@
     if (self.detailItem) {
         self.detailDescriptionLabel.text = [self.detailItem description];
     }
+    
+    
+    
+    [self setupTestBorder];
+    
+    
+    
+}
+
+- (void)setupTestBorder{
+    
+    CGFloat itemWidth = CGRectGetWidth((self.view.frame))/3;
+    CGFloat itemHeight = 100;
+    
+    int maxRow = 2;
+    int maxCol = 3;
+    
+    for (int row = 0; row < maxRow; row++) {
+        for (int col = 0; col < maxCol; col++) {
+            
+            UIView *testBorderView = [[UIView alloc] initWithFrame:CGRectMake(col*(itemWidth), 100+row*itemHeight, itemWidth, itemHeight)];
+            CALayer *layer = testBorderView.layer;
+            layer.shadowColor = [UIColor blackColor].CGColor;
+            layer.shadowOpacity = 0.0f;
+            layer.shadowOffset = CGSizeZero;
+            
+            testBorderView.backgroundColor = [UIColor whiteColor];
+            // Test
+            
+            NSDictionary *styleTop = @{
+                                       @"width":@(1),
+                                       @"inset":NSStringFromUIEdgeInsets(UIEdgeInsetsMake(0, 0, 0, 0)),
+                                       @"color":[UIColor redColor]
+                                       };
+            
+            if (row == 0) {
+                [testBorderView setBorderWithStyle:styleTop forEdge:UIViewBorderEdgeTop];
+            }
+            
+            /*
+            NSDictionary *styleLeft = @{
+                                        @"width":@(1),
+                                        @"inset":NSStringFromUIEdgeInsets(UIEdgeInsetsMake((row==0)?10:0, 0, (row==maxRow-1)?10:0, 0)),
+                                        @"color":[UIColor greenColor]
+                                        };
+            
+            if (col == 0) {
+                [testBorderView setBorderWithStyle:nil forEdge:UIViewBorderEdgeLeft];
+            }else{
+                [testBorderView setBorderWithStyle:styleLeft forEdge:UIViewBorderEdgeLeft];
+            }
+            */
+            
+            NSDictionary *styleRight = @{
+                                         @"width":@(1),
+                                         @"inset":NSStringFromUIEdgeInsets(UIEdgeInsetsMake((row==0)?10:0, 0, (row==maxRow-1)?10:0, 0)),
+                                         @"color":[UIColor blueColor]
+                                         };
+            if (col==maxCol-1) {
+                [testBorderView setBorderWithStyle:nil forEdge:UIViewBorderEdgeRight];
+            }else{
+                [testBorderView setBorderWithStyle:styleRight forEdge:UIViewBorderEdgeRight];
+            }
+            NSDictionary *styleBottom = @{
+                                          @"width":@(1),
+                                          @"inset":NSStringFromUIEdgeInsets(UIEdgeInsetsMake(0, 0, 0, 0)),
+                                          @"color":[UIColor purpleColor]
+                                          };
+            
+            [testBorderView setBorderWithStyle:styleBottom forEdge:UIViewBorderEdgeBottom];
+            
+            [self.view addSubview:testBorderView];
+            
+        }
+    }
+    
 }
 
 - (void)viewDidLoad {
